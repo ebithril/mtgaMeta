610094c8224453f4d414a8a670be3c9597d3e3a6
Patrik Görling <p.gorling@gmail.com>
2018-05-22 21:10:55 +0200

    First commit

---
+++ server.py
@@ -0,0 +1,25 @@
+from mtgsdk import Card
+
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+    
+
+@app.route('/search', methods=['POST'])
+def search():
+    sets = 'AER,AKH,HOU,XLN,RIX,DOM'
+
+    names = []
+    cards = Card.where(set=sets).where(name=request.form['search']).all()
+    for card in cards:
+        names.append(card.name)
+
+    return jsonify(names)
+
